437b1a333e1fbb1c35f121ec107bd67f96f7507e
samgamerisbest <samar.walkar42@gmail.com>
2026-09-08 17:52:24 +0530

    Add stock and simulation result structures

--- types.hpp
+++ types.hpp
@@ -1,1 +1,24 @@
+#pragma once
+#include <string>
+#include <vector>
+struct stock
+{
+    std::string name;
+    int shares;
+    double  price;
+};
 
+struct stockstats
+{
+    double sigma;  // volatily
+    double mu;     // mean
+};
+struct simulationresult
+{
+    double prob; //probability of success
+    double median; // median value of the simulations
+    double p5; //5th percentile value
+    double p95; //95th percentile value
+    std::vector<double>  all_results; 
+
+};
